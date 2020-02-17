547cfb30185b25d20236bed6f0b1ad0db3c10742
ethernetcat <ethernetcat@protonmail.com>
2020-02-18 03:02:43 +0900

    Add inventory_menu

--- main.py
+++ main.py
@@ -1,21 +1,40 @@
 from BarcodeReading import *
+from InventoryManager import check_inventory_list
 
-def main():
+def menu():
     while True:
-        print("Menu: Read(R), Check(C), Exit(E)")
+        print("Menu: Read(R), Inventory(I), Exit(E)")
         x = input("Type: ")
         if x == "Read" or x == "R":
             Read()
-        elif x == "Check" or x == "C":
-            print("Check")
+        elif x == "Inventory" or x == "I":
+            inventory_menu()
         elif x == "Exit" or x == "E":
+            break
+        else:
+            print("[ERROR] INCORRECT")
+
+def inventory_menu():
+    while True:
+        print("Menu: List(L), Add(A), Back(B)")
+        x = input("Type: ")
+        if x == "List" or x == "L":
+            check_inventory_list()
+        elif x == "Add" or x == "A":
+            print("Add item")
+            JAN = input("JAN: ")
+            NAME = input("NAME: ")
+            PRICE = input("PRICE: ")
+            STOCK_QUANTITY = input("STOCK QUANTITY: ")
+            add_inventory_item(JAN, NAME, PRICE, STOCK_QUANTITY)
+        elif x == "Back" or x == "B":
             break
         else:
             print("[ERROR] INCORRECT")
 
 if __name__ == "__main__":
     try:
-        main()
+        menu()
         pass
     except KeyboardInterrupt:
         pass
