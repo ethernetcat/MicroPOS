14c2ffc2ab74e4a63f92bdcb65c75695c147a07a
ethernetcat <ethernetcat@protonmail.com>
2020-02-18 15:42:03 +0900

    Add get_purchased_csv function

--- Read.py
+++ Read.py
@@ -4,3 +4,8 @@ def get_items_csv():
     with open("items.csv", encoding="utf_8") as file:
         items = list(csv.reader(file))
     return items
+
+def get_purchased_csv():
+    with open("purchased.csv", encoding="utf_8") as file:
+        purchaced = list(csv.reader(file))
+    return purchaced
